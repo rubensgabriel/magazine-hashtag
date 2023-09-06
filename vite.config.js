12974de886c5507002911977b3065b01c714988d
Rubens Gabriel <rubens-ita@hotmail.com.br>
2023-09-06 15:16:07 -0300

    ajuste

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,5 @@
 import {defineConfig} from 'vite'
-import tailwindcss from '@vitejs/plugin-tailwindcss'
 
 export default defineConfig({
-    plugins: [tailwindcss()],
     base: "/magazine-hashtag"
 })
