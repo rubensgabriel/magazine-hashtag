b5d8b90137007e848996487dedece9bc4b296ac4
Rubens Gabriel <96795829+rubensgabriel@users.noreply.github.com>
2023-09-06 16:15:43 -0300

    inclusao das demais paginas

---
+++ checkout.js
@@ -0,0 +1,39 @@
+import { atualizarPrecoCarrinho } from "./src/menuCarrinho";
+import {
+    desenharProdutoCarrinhoSimples,
+    lerLocalStorage,
+    apagarDoLocalStorage, 
+    salvarLocalStorage } from "./src/utilidades";
+
+function desenharProdutosCheckout () {
+    const idsProdutoCarrinhoComQuantidade = lerLocalStorage('carrinho') ?? {};
+    for(const idProduto in idsProdutoCarrinhoComQuantidade) {
+        desenharProdutoCarrinhoSimples(idProduto, 'container-produtos-checkout', idsProdutoCarrinhoComQuantidade[idProduto]);
+    }
+    atualizarPrecoCarrinho();
+}
+
+function finalizarCompra (evento) {
+    evento.preventDefault();
+    const idsProdutoCarrinhoComQuantidade = lerLocalStorage('carrinho') ?? {};
+    if(Object.keys(idsProdutoCarrinhoComQuantidade).length === 0) {
+         return;
+    }
+
+    const dataAtual = new Date();
+    const pedidoFeito = {
+         dataPedido: dataAtual,
+         pedido: idsProdutoCarrinhoComQuantidade
+    }
+    const historicoDePedidos = lerLocalStorage('historico') ?? [];
+    const historicoDePedidosAtualizado = [pedidoFeito, ...historicoDePedidos]
+
+    salvarLocalStorage('historico', historicoDePedidosAtualizado)
+    apagarDoLocalStorage('carrinho');
+
+    window.location.href = window.location.origin + "/pedidos.html";
+}
+
+desenharProdutosCheckout();
+
+document.addEventListener('submit', (evt) => finalizarCompra(evt));
